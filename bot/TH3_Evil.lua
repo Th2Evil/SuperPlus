ff1d5721717a7f3af6c4de9d3faa191d3a771aba
blackops97 <exetra29988@gmail.com>
2016-08-14 00:50:47 +0300

    Update TH3_Evil.lua

--- bot/TH3_Evil.lua
+++ bot/TH3_Evil.lua
@@ -5,7 +5,7 @@ package.cpath = package.cpath .. ';.luarocks/lib/lua/5.2/?.so'
 require("./bot/utils")
 
 local f = assert(io.popen('/usr/bin/git describe --tags', 'r'))
-VERSION = assert(f:read('*a'))
+VERSION = assert(f:read('*a')) 
 f:close()
 
 -- This function is called when tg receive a msg

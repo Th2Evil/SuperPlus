caab962182165003165d6f3370df3512e91ca05d
Th2Evil <sudo6123@gmail.com>
2016-09-08 21:16:29 +0300

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -10,22 +10,22 @@
 do
 
 local function run(msg, matches)
-  if matches[1] ==  موقعي  then
+  if matches[1] == 'موقعي' then
     if is_sudo(msg) then
     send_document(get_receiver(msg), "./files/me/sudo.webp", ok_cb, false)
-      return  انت المطور مالتي 😻🙊 .. \n\n .. 🔶MSG BY telegram.me/ ..msg.from.username.. \n 
+      return 'انت المطور مالتي 😻🙊'..'\n\n'..'🔶MSG BY telegram.me/'..msg.from.username..'\n'
     elseif is_admin1(msg) then
     send_document(get_receiver(msg), "./files/me/admin.webp", ok_cb, false)
-      return  ادمن ادمن بلكروب 🌚  .. \n\n .. 🔶MSG BY telegram.me/ ..msg.from.username.. \n 
+      return 'ادمن ادمن بلكروب 🌚'..'\n\n'..'🔶MSG BY telegram.me/'..msg.from.username..'\n'
     elseif is_owner(msg) then
     send_document(get_receiver(msg), "./files/me/leader.webp", ok_cb, false)
-      return   نت مشرف الكروب🐸💭 .. \n\n .. 🔶MSG BY telegram.me/ ..msg.from.username.. \n 
+      return 'نت مشرف الكروب🐸💭'..'\n\n'..'🔶MSG BY telegram.me/'..msg.from.username..'\n'
     elseif is_momod(msg) then
     send_document(get_receiver(msg), "./files/me/moderator.webp", ok_cb, false)
-      return  انت طايح حظك😹 .. \n\n .. 🔶MSG BY telegram.me/ ..msg.from.username.. \n 
+      return 'انت طايح حظك😹'..'\n\n'..'🔶MSG BY telegram.me/'..msg.from.username..'\n'
     else
     send_document(get_receiver(msg), "./files/me/member.webp", ok_cb, false)
-      return  انت عضـو لا تلح زايــد .  \n\n .. 🔶MSG BY telegram.me/ ..msg.from.username.. \n 
+      return 'انت عضـو لا تلح زايــد'..'\n\n'..'🔶MSG BY telegram.me/'..msg.from.username..'\n'
     end
   end
 end
@@ -33,7 +33,7 @@ end
 return {
   patterns = {
     "^(موقعي)$",
-    "^(موقعي)$"
+    "^(موقعي)$",
     },
   run = run
 }

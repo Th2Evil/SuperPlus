a3d3b0128853bc7c9d0a4f5d5ce7b3a9814cfffb
Th2Evil <sudo6123@gmail.com>
2016-09-09 02:04:10 +0300

    Update nedme.lua

--- plugins/nedme.lua
+++ plugins/nedme.lua
@@ -1,16 +1,16 @@
 do
 
  function mohammed(msg, matches)
- local ch = '226861861'..msg.to.id--هنا حط ايديك
- local fuse = 'طلب احدهم مجموعه🌚\n\n🔹️ايديه ' .. msg.from.id .. '\n\nاسمه🔹️ ' .. msg.from.print_name ..'\n\nمعرفه🔹️ @' .. msg.from.username ..'\n\nايدي المجموعه  🔹️ '..msg.to.id.. '\n\n🔹️طلب منك؟\n' .. matches[1]
+ local ch = '230410522'..msg.to.id--هنا حط ايديك
+ local fuse = 'طلب احدهم مجموعه🌚\n\n🔹️ايديه ' .. msg.from.id .. '\n\nاسمه🔹️ ' .. msg.from.print_name ..'\n\nمعرفه🔹️ telegram.me/' .. msg.from.username ..'\n\nايدي المجموعه  🔹️ '..msg.to.id.. '\n\n🔹️طلب منك؟\n' .. matches[1]
  local fuses = '!printf user#id' .. msg.from.id
 
 
    local text = matches[1]
-   local chat = "user#id"..226861861--هنا حط ايديك
+   local chat = "user#id"..230410522--هنا حط ايديك
 
   local sends = send_msg(chat, fuse, ok_cb, false)
-return 'تم ارسال طلبك لصنع مجموعه الى المطور \n @TH3BOSS\nالرجاء انتضار المطور لصنع مجموعه لك عزيزي \n\n@'..msg.from.username
+return 'تم ارسال طلبك لصنع مجموعه الى المطور \n @Th2_BOOS\nالرجاء انتضار المطور لصنع مجموعه لك عزيزي \n\ntelegram.me/'..msg.from.username
 end
  end
  return {

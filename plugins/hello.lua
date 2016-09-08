2693a8a7529a913a0ed5a74061f94fdc94d60c0c
Th2Evil <sudo6123@gmail.com>
2016-09-09 00:34:34 +0300

    Update hello.lua

--- plugins/hello.lua
+++ plugins/hello.lua
@@ -1,37 +1,35 @@
 --[[ 
-▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
-▀▄ ▄▀                                      ▀▄ ▄▀ 
-▀▄ ▄▀    BY MOHAMMED HISHAM                ▀▄ ▄▀ 
-▀▄ ▄▀     BY MOHAMMEDHISHAM (@TH3BOSS)     ▀▄ ▄▀ 
-▀▄ ▄▀ JUST WRITED BY MOHAMMED HISHAM       ▀▄ ▄▀   
-▀▄ ▄▀                                      ▀▄ ▄▀ 
-▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
---]]
-
-do
-    
-local function mohammedboss(msg,matches)
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀ 
+▀▄ ▄▀                                       ▀▄ ▄▀ 
+▀▄ ▄▀       BY waleed-khalid-ali              ▀▄ ▄▀ 
+▀▄ ▄▀ BY waleed_khalid (@Th2_BOOS)          ▀▄ ▄▀ 
+▀▄ ▄▀ JUST WRITED BY @Th2_BOOS        ▀▄ ▄▀ 
+▀▄ ▄▀          Hello  : الترحـــيب              ▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀ 
+--]] 
+do 
+local function mohammedboss(msg,matches) 
     if matches[1] == "chat_add_user"  then 
-      return "▫️ اهـلا بـك عـزيـزي ❤️ \n".."▫️ اسم المجموعة :: "..msg.to.title.."\n".." ▫️ايدي المجموعة :: "..msg.to.id.."\n".."▫️ اسم الضافك ::"..(msg.from.first_name or " ").."\n".."▫️ معرف الضافك :: telegram.me/"..(msg.from.username or " ").."\n".."▫️ معرفك :: telegram.me/"..(msg.action.user.username or "لايوجد " ).."\n".."▫️ ايدي الضافك ::"..msg.from.id
+      return "✺ اهـلا بـك عـزيـزي ❤️ \n".."♾ اسم المجموعة :: "..msg.to.title.."\n".." ∅ايدي المجموعة :: "..msg.to.id.."\n".."☠ اسم الضافك ::"..(msg.from.first_name or " ").."\n".."☢ معرف الضافك :: @"..(msg.from.username or " ").."\n".."☣ معرفك :: telegram.me/"..(msg.action.user.username or "لايوجد " ).."\n".."✇ ايدي الضافك ::"..msg.from.id 
 
-    elseif matches[1] == "chat_add_user_link" then
-      return "▫️اهـلا بـك عـزيـزي ❤️\n".."▫️ اسم المجموعة :: "..msg.to.title.."\n".."▫️ ايدي المجموعة :: "..msg.to.id.."\n".."▫️ اسم الضافك :: "..(msg.from.first_name or " ").."\n".."▫️ معرف الي ضافك :: telegram.me/"..(msg.from.username or " ").."\n".."▫️ معرفك :: telegram.me/"..(msg.from.username or "لايوجد " ).."\n".." ▫️ايديك ::"..msg.from.id
+    elseif matches[1] == "chat_add_user_link" then 
+      return "♥اهـلا بـك عـزيـزي ❤️\n".."☆ اسم المجموعة :: "..msg.to.title.."\n".."⚛ ايدي المجموعة :: "..msg.to.id.."\n".."♽ اسم الضافك :: "..(msg.from.first_name or " ").."\n".."⚶ معرف الي ضافك :: @"..(msg.from.username or " ").."\n".."☤ معرفك :: telegram.me/"..(msg.from.username or "لايوجد " ).."\n".." ☂ايديك ::"..msg.from.id 
 
-    end
-    if matches[1] == "chat_del_user" then
-    return "روحه بلا رده اكو غيرك يــجينه"..msg.action.user.first_name
-end
-end
-return {
-    patterns = {
-        "^!!tgservice (chat_add_user)$",
-        "^!!tgservice (chat_add_user_link)$",
-        "^!!tgservice (chat_del_user)$"
-       
-    },
- run = mohammedboss,
-}
-end
--- Dev💀: @TH3BOSS
--- Dev bot💀: @ll60Kllbot
--- DevChannel 💀: @llDEV1ll
+    end 
+    if matches[1] == "chat_del_user" then 
+    return "⚠✌للاســف انك غادرت كنا نسعـد ببقائـك معنـا☝⚠"..msg.action.user.first_name 
+end 
+end 
+return { 
+    patterns = { 
+        "^!!tgservice (chat_add_user)$", 
+        "^!!tgservice (chat_add_user_link)$", 
+        "^!!tgservice (chat_del_user)$" 
+    }, 
+ run = mohammedboss, 
+} 
+end 
+-- Dev☣ @Th2_BOOS
+-- Dev bot☢ @leloobot
+-- DevChannel ☠ @Queenlove20
+

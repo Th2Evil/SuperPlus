483cc17162b5619cc68cec68a154b4c74bafb15f
Th2Evil <sudo6123@gmail.com>
2016-09-08 12:38:12 +0300

    Update VIRSON.lua

--- plugins/VIRSON.lua
+++ plugins/VIRSON.lua
@@ -12,7 +12,7 @@
   
   function mohammed(msg, matches)
   local reply_id = msg['id']
-    local S = ' ▫️     السورس    Th2BOOS📁\n\n▫️     الاصدار 📋 V1 \n\n▫️     الموقع \n \nhttps://github.com/Th2BOOS/SuperPlus\n\n▫️     المطور : @Th2_BOOS \n\n▫️   المطور2  :  @Bedo_Prog \n \n▫️   قـنـاه الـسـورس :  https://telegram.me/joinchat/DbvJGj753M_fc5b9uppC8A '  reply_msg(reply_id, S, ok_cb, false)
+    local S = ' ▫️     السورس    Th2Evil📁\n\n▫️     الاصدار 📋 V1 \n\n▫️     الموقع \n \ngit clonehttps://github.com/Th2Evil/SuperPlus.git\n\n▫️     المطور : @Th2_BOOS \n\n▫️   المطور2  :  @Bedo_Prog \n \n▫️   قـنـاه الـسـورس :  https://telegram.me/joinchat/DbvJGj753M_fc5b9uppC8A '  reply_msg(reply_id, S, ok_cb, false)
   end
   
   return {
